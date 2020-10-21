8bcf1cba64afd750495434bd662ff069ff90a77d
Elijah Flanders <elijahflanders15@gmail.com>
2020-10-21 15:54:57 -0500

    created a server.js file and set up basic server functionality

---
+++ Develop/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+let PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use( express.static('public'));
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT", PORT);
+})
