8c91be3370767eabbbbdc678a60dd7238badbece
Elijah Flanders <elijahflanders15@gmail.com>
2020-10-21 16:10:55 -0500

    routed the html files

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 let PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use( express.static('public'));
+
+//HTML ROUTES
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../develop/public/notes.html"))
+})
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+})
 
 app.listen(PORT, function() {
     console.log("App listening on PORT", PORT);
