6786575fbf05138ac782dc4b77ff0de4d1a2471e
Elijah Flanders <elijahflanders15@gmail.com>
2020-10-21 20:36:33 -0500

    wrote the api routes, jsut need to get the delete function to work properly

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
+const db = require("../develop/db/db.json");
 
 let PORT = process.env.PORT || 3000;
 
@@ -17,6 +19,30 @@ app.get("/notes", function(req, res) {
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"))
 })
+
+//API ROUTES
+
+app.get("/api/notes", function(req, res) {
+    res.json(db)
+});
+
+var i = 1;
+
+app.post("/api/notes", function(req, res) {
+    req.body.id = i++
+    db.push(req.body);
+    fs.writeFileSync("../develop/db/db.json", JSON.stringify(db));
+    res.json(req.body);
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+    db.splice(req.params.id, 1);
+    console.log(req.params.id); 
+    fs.writeFileSync("../develop/db/db.json", JSON.stringify(db));
+    res.status(200).end();
+});
+
+//Set the PORT up to listen
 
 app.listen(PORT, function() {
     console.log("App listening on PORT", PORT);
